9cecfc4b5053663e36bf309e7a068e09f07bbac7
Kang-dot <jgbu97@gmail.com>
2025-10-14 15:16:33 +0900

    [TOOLS-4773] apply formatter

--- plugins/com.cubrid.cubridmigration.ui/src/com/cubrid/cubridmigration/ui/wizard/page/view/SQLTableManageView.java
+++ plugins/com.cubrid.cubridmigration.ui/src/com/cubrid/cubridmigration/ui/wizard/page/view/SQLTableManageView.java
@@ -617,7 +617,10 @@ public class SQLTableManageView extends AbstractMappingView {
             Catalog catalog = config.getTarCatalog().orElse(config.getSrcCatalog());
 
             if (catalog != null && catalog.getSchemas() != null) {
-            	tarSchemaList = catalog.getSchemas().stream().map(Schema::getTargetSchemaName).toArray(String[]::new);
+                tarSchemaList =
+                        catalog.getSchemas().stream()
+                                .map(Schema::getTargetSchemaName)
+                                .toArray(String[]::new);
             }
         }
 
